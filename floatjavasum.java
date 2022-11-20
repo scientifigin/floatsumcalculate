4e8d838509da4989d5395636fd88261e5f9b0512
GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>
2022-11-20 19:28:26 +0530

    Create floatjavasum.java
    Signed-off-by: GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>

---
+++ floatjavasum.java
@@ -0,0 +1,87 @@
+// For Addition 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+        float b = sc.nextFloat();
+//        int sum = a+b;
+        float sum = a+b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For substruction 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+        float b = sc.nextFloat();
+//        int sum = a+b;
+        float sum = a-b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For Multiplication 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+        float b = sc.nextFloat();
+//        int sum = a+b;
+        float sum = a*b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+// For Division 
+package scientifigin;
+import java.util.Scanner;
+
+public class Thirdjava {
+    public static void main(String[] arg) {
+        System.out.println("Taking Input From The User");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+//        int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2");
+//        int b = sc.nextInt();
+        float b = sc.nextFloat();
+//        int sum = a+b;
+        float sum = a/b;
+        System.out.println("The sum of these number is");
+        System.out.println(sum);
+    }
+}
+
+
+
+
